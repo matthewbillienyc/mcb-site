50f9d24db4f3d7527de1e3b6a6c66b839bdb7569
matthewbillienyc <matthewbillienyc@gmail.com>
2015-12-28 17:50:07 -0500

    asshole pipeline hopefully fixed

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -8,3 +8,8 @@ Rails.application.config.assets.version = '1.0'
 
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
+Rails.application.config.assets.precompile += %w( master.css nav.js )
+Rails.application.config.assets.precompile += %w( artistdata/artist.js artistdata/build-chart.js artistdata/data-miner.js artistdata/spotify-chart.js artistdata/artistdata.css d3.v3.js jquery.min.js )
+Rails.application.config.assets.precompile += %w( counterpoint/counterpoint.js counterpoint/counterpoint.css )
+Rails.application.config.assets.precompile += %w( doubletheremin/doubletheremin.js doubletheremin/doubletheremin.css )
+Rails.application.config.assets.precompile += %w( tonebox/tonebox.css tonebox/tonebox.js )
