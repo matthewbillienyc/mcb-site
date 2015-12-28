6ae9024a420471f035e93396c8b05f8af308b761
matthewbillienyc <matthewbillienyc@gmail.com>
2015-12-28 16:20:33 -0500

    commit

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -8,4 +8,3 @@ Rails.application.config.assets.version = '1.0'
 
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
-Rails.application.config.assets.precompile += %w( nav.js jquery.min.js d3.v3.js tonebox/tonebox.js doubletheremin/doubletheremin.js counterpoint/counterpoint.js artistdata/artistdata.js artistdata/artist.js artistdata/build-chart.js artistdata/data-miner.js artistdata/spotify-chart.js artistdata/artistdata.css counterpoint/counterpoint.css doubletheremin/doubletheremin.css tonebox/tonebox.css master.css )
